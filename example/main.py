911f7266668d4e0daf76fefce96b51c5f717bbf8
Mohammad Amin Ghasemi <62441713+Amin-Ghasemi@users.noreply.github.com>
2023-04-30 10:52:10 +0000

    Update main.py

--- example/main.py
+++ example/main.py
@@ -1,6 +1,6 @@
 from menu import Menu
 
-items = ['option 0', 'option 1', 'option 2', 'option 3', 'option 4', 'EXIT']
+items = ['option 0', 'option 1', 'option 2', 'option 3', 'option 4', 'option 5']
 
 menu = Menu(items)
 
@@ -8,9 +8,22 @@ output_panel = menu.load_menu()
 
 if output_panel == 0:
     print(f'You Selected Option -> {output_panel}')
+
 elif output_panel == 1:
     print(f'You Selected Option -> {output_panel}')
+
 elif output_panel == 2:
     print(f'You Selected Option -> {output_panel}')
-else:
+
+elif output_panel == 3:
+    print(f'You Selected Option -> {output_panel}')
+
+elif output_panel == 4:
+    print(f'You Selected Option -> {output_panel}')
+
+elif output_panel == 5:
+    print(f"you exited...")
+    menu.while_check[0] = False
     exit()
+else:
+    pass
