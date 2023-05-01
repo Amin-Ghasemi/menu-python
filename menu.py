b33611b66b323430ea2a05b884b59e4de52a9b0f
Mohammad Amin Ghasemi <62441713+Amin-Ghasemi@users.noreply.github.com>
2023-05-01 13:30:15 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -15,6 +15,7 @@ class Menu:
         self.while_check = [True]
         self.console_size = shutil.get_terminal_size()
         self.console_width = self.console_size.columns
+
         for i in range(len(list_options)):
             self.options[i] = f'{list_options[i]}'.center(self.console_width)
 
@@ -22,17 +23,19 @@ class Menu:
         list_options_str = ''
         for o in self.options:
             if o == self.selected_option:
-                list_options_str += f"{Back.LIGHTWHITE_EX}{Fore.BLACK}{self.options.get(o)}{Back.BLACK}{Fore.LIGHTWHITE_EX}\n"
+                list_options_str += f"{Back.LIGHTWHITE_EX}{Fore.BLUE}{self.options.get(o)}{Back.BLUE}{Fore.LIGHTWHITE_EX}\n"
             else:
-                list_options_str += f"{Back.BLACK}{Fore.LIGHTWHITE_EX}{self.options.get(o)}{Back.LIGHTWHITE_EX}{Fore.BLACK}\n"
+                list_options_str += f"{Back.BLUE}{Fore.LIGHTWHITE_EX}{self.options.get(o)}{Back.LIGHTWHITE_EX}{Fore.BLUE}\n"
+
         return list_options_str
 
     def load_menu(self):
         system("clear")
+        system("stty -echo")
 
         print(self.load_list_options())
 
-        help_ = f'{Fore.LIGHTWHITE_EX}{Back.BLUE}Use Left < Or > Right and Ctrl for Select'
+        help_ = f'{Fore.LIGHTWHITE_EX}{Back.BLUE}Use Left < Or > Right and Space for Select'
         print(help_)
 
         while self.while_check[0]:
@@ -53,8 +56,9 @@ class Menu:
                     self.selected_option += 1
                 print(self.load_list_options())
                 print(help_)
-            elif key == 'ctrl':
+            elif key == 'space':
+                system("stty echo")
                 return self.selected_option
-            sleep(.2)
+            sleep(.1)
 
         return self.selected_option
